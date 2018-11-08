a18e0fea7400d2521a253a95254d0985b052def0
Nicci Lee Abueg <nicci.abueg@gmail.com>
2018-11-08 09:54:09 -0800

    update

---
+++ 01-javascript/02-js-fundamentals/04-trivia.js
@@ -0,0 +1,29 @@
+// function counter(){
+//     var count = 0;
+//     function Child(){
+//         console.log('count ', count);
+//         count++;
+//         return count++;
+//     }
+//     return Child;
+// }
+// counter = counter();
+// console.log(counter());
+
+// function counter(){
+//     var points = 0;
+//     points = points + 100; //must be a variable for either 100, 200 or 300 points based on row?
+//     console.log(points);
+//     return points;
+// }
+
+// var x = document.getElementById("data").getElementsByTagName("a").length;
+
+// function myFunction() {
+//     var x = document.getElementById('a');
+//     if (x.style.visibility === 'hidden') {
+//         x.style.visibility = 'visible';
+//     } else {
+//         x.style.visibility = 'hidden';
+//     }
+// }
